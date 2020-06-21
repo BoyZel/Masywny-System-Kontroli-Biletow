b6a551b7db83167f7efd7f580c77e43696210eb6
mkalino1 <mateusz.kalinowski1998@gmail.com>
2020-06-21 14:11:14 +0200

    Inner menu

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from functions import *
 conn = sqlite3.connect('database.db')
 
 choice = '0'
+inner_choice = '0'
 while choice != 'q':
     print("Main Choice: Choose 1 of 5 choices")
-    print("Choose 1 for pojazd id")
+    print("Zarzadzanie mandatami")
     print("Choose 2 for something")
     print("Choose 3 for something")
     print("Choose 4 for something")
@@ -18,7 +19,20 @@ while choice != 'q':
     choice = input("Please make a choice: ")
 
     if choice == "1":
-        pojazd_id(conn)
+        print("    Wybierz 1 aby wprowadzic mandat")
+        print("    Wybierz 2 aby usunac mandat")
+        print("    Wybierz 3 aby wyswietlic mandaty klienta")
+
+        inner_choice = input("    Please make a choice: ")
+        if inner_choice == "1":
+            print("    wproawdzam mandat")
+        if inner_choice == "2":
+            print("    usuwam mandat")
+        if inner_choice == "3":
+            print("    wproawdzam mandat")
+        else:
+            print("    Nie rozpoznano polecenia")
+
     elif choice == "2":
         print("Do Something 2")
     elif choice == "3":
